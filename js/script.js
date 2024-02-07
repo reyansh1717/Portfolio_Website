9b89f49841dad5b02287ea162013d5a066076a9e
reyansh1717 <reyansh170@gmail.com>
2024-02-08 02:58:43 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*=====TYPING ANIMATION=====*/
+var typed = new Typed(".typing",{
+    strings:["","Web developer!","Web designer!", "Programmer!", "Photographer!", "Campus Ambassador!"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
